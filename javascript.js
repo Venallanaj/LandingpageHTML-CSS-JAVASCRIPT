599155b1a4a920c747784743f48e0596453932f9
Frontenddev123 <vena.llanaj@outlook.com>
2023-03-26 18:42:03 +0200

    adds-changes

--- javascript.js
+++ javascript.js
@@ -1,25 +1,6 @@
 // FUNCTION Search
 
-function myFunction() {
-  let filter, ul, li, a, i, txtValue;
-  input = document.getElementById("myInput");
-  filter = input.value.toUpperCase();
-  ul = document.getElementById("myUL");
-  li = ul.getElementsByTagName("li");
-
-  for (i = 0; i < li.length; i++) {
-    a = li[i].getElementsByTagName("a")[0];
-    txtValue = a.textContent || a.innerText;
-    if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      li[i].style.display = "";
-    } else {
-      li[i].style.display = "none";
-    }
-  }
-}
-
 function searchData() {
-  debugger;
   let filter, buttons, a, b, txtValue;
   input = document.getElementById("searchInput");
   filter = input.value.toUpperCase();
@@ -37,32 +18,26 @@ function searchData() {
   }
 }
 
-const cardSliders = document.querySelectorAll(".card-slider");
+let slideIndex = [1, 1];
+let slideId = ["mySlides1", "mySlides2"];
+showSlides(1, 0);
+showSlides(1, 1);
+debugger;
+function plusSlides(n, no) {
+  showSlides((slideIndex[no] += n), no);
+}
 
-cardSliders.forEach((slider) => {
-  let isDown = false;
-  let startX;
-  let scrollLeft;
-
-  slider.addEventListener("mousedown", (e) => {
-    isDown = true;
-    startX = e.pageX - slider.offsetLeft;
-    scrollLeft = slider.scrollLeft;
-  });
-
-  slider.addEventListener("mouseleave", () => {
-    isDown = false;
-  });
-
-  slider.addEventListener("mouseup", () => {
-    isDown = false;
-  });
-
-  slider.addEventListener("mousemove", (e) => {
-    if (!isDown) return;
-    e.preventDefault();
-    const x = e.pageX - slider.offsetLeft;
-    const walk = (x - startX) * 3;
-    slider.scrollLeft = scrollLeft - walk;
-  });
-});
+function showSlides(n, no) {
+  let i;
+  let x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {
+    slideIndex[no] = 1;
+  }
+  if (n < 1) {
+    slideIndex[no] = x.length;
+  }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex[no] - 1].style.display = "block";
+}
